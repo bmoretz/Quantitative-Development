cd462207ab6797b49446714487ba74c8c269e834
Brandon Moretz <bmoretz82@gmail.com>
2021-04-24 18:57:25 -0400

    vector problems
    worked through a few vector problems

--- matlib/vectors/transforms.py
+++ matlib/vectors/transforms.py
@@ -1,10 +1,37 @@
 from matlib.vectors.types import Vector
 
 def shift(vectors : list[Vector], x : float, y : float) -> Vector:
+    """shifts / translates a set of vectors.
+
+    Args:
+        vectors (list[Vector]): input vectors
+        x (float): x shift
+        y (float): y shit
+
+    Returns:
+        Vector: mutated vectors
+    """
     return [Vector(v.x + x, v.y + y) for v in vectors]
 
 def scale(vectors : list[Vector], k : float):
+    """scales a set of vectors.
+
+    Args:
+        vectors (list[Vector]): input vectors.
+        k (float): amount to scale.
+
+    Returns:
+        [type]: mutated vectors
+    """
     return [Vector(v.x*k, v.y*k) for v in vectors]
 
 def flip(vectors : list[Vector]):
+    """flips a set of vectors.
+
+    Args:
+        vectors (list[Vector]): input vectors
+
+    Returns:
+        [type]: mutated vectors
+    """
     return [Vector(-v.x, -v.y) for v in vectors]
--- scratch.py
+++ scratch.py
@@ -10,9 +10,19 @@ dino_vectors = make_vectors([(6,4), (3,1), (1,2), (-1,5), (-2,5), (-3,4), (-4,4)
     (-5,3), (-5,2), (-2,2), (-5,1), (-4,0), (-2,1), (-1,0), (0,-3),
     (-1,-4), (1,-4), (2,-3), (1,-2), (3,-1), (5,1)])
 
-dino_vectors3 = flip(dino_vectors)
+shapes = []
 
-draw_shapes(
-    Polygon(*dino_vectors, color=Color.Blue),
-    Polygon(*dino_vectors3, color=Color.Orange)
-)
+y_offset = 10
+for row in range(0, 10):
+    
+    x_offset = 2
+
+    for col in range(0, 10):
+        scaled = scale(dino_vectors, .1)
+        transformed = shift(scaled, x_offset, y_offset)
+        shapes += [Polygon(*transformed, color=Color.Blue)]
+        x_offset = x_offset + 2
+
+    y_offset = y_offset - 1
+
+draw_shapes(*shapes)
